3ab976fca68d38227a2d53777ad7a0ee80e18df5
aromi2 <quf0115@handong.ac.kr>
2023-05-19 00:36:23 +0900

    UsingBufferedReader Commit

--- app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -1,6 +1,9 @@
 package edu.handong.csee.java.example.fileread.benchmark;
 
 import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.EOFException;
 
 /**
  * Created by sherxon on 4/23/17. https://github.com/sherxon/AlgoDS/tree/master/src/oi
@@ -11,28 +14,47 @@ public class UsingBufferedReader {
 
         //-------------- Test reading 1 MB file. --------------------
 
-        StopWatch.start();
+    	try {
+    		StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
-        while (inputStream.read()!=-1){}
+            BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+            while (inputStream.read()!=-1){}
 
-        long duration = StopWatch.stop();
-        System.out.println(duration + " milsec");
+            long duration = StopWatch.stop();
+            System.out.println(duration + " milsec");
+            
+            inputStream.close();
+    		
+    	}catch(FileNotFoundException e) {
+			System.out.println("Problem opening the file input1MB");
+		} catch (EOFException e) {
+			System.out.println("Problem reading the file input1MB");
+			System.out.println("Reached end of the file.");
+		} catch (IOException e) {
+			System.out.println("Problem reading the file input1MB");
+		}
         
-        inputStream.close();
-
 
         //-------------- Test reading 10 MB file. --------------------
+    	try {
+    		 StopWatch.start();
 
-        StopWatch.start();
+	        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+	        while (inputStream2.read()!=-1){}
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
-        while (inputStream2.read()!=-1){}
+	        long duration2 = StopWatch.stop();
+	        System.out.println(duration2 + " milsec");
 
-        long duration2 = StopWatch.stop();
-        System.out.println(duration2 + " milsec");
-
-        inputStream2.close();
+	        inputStream2.close();
+    	}catch(FileNotFoundException e) {
+			System.out.println("Problem opening the file input1MB");
+		} catch (EOFException e) {
+			System.out.println("Problem reading the file input1MB");
+			System.out.println("Reached end of the file.");
+		} catch (IOException e) {
+			System.out.println("Problem reading the file input1MB");
+		}
+       
 
         /*
         //-------------- Test reading 100 MB file. --------------------
